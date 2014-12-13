3bc0037cf74a579fdfefdd1210616c5f1c22c4df
ckl <imcookielau@gmail.com>
2014-12-13 17:00:40 +0800

    Complete the code.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions
+##  makeCacheMatrix - Create a CacheMatrix object that 
+##                    the inverse of the matrix is cacheable.
+##  cacheSolve      - Retrieve the inverse of a matrix 
+##                    from CacheMatrix object.
+##
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Create a CacheMatrix object that the inverse of the matrix is cacheable.
+makeCacheMatrix <- function(...) {
+    mtrx <- matrix(...)
+    ivm <- NULL
+    getmtrx <- function() mtrx
+    setivm <- function(y) {
+        ivm <<- y
+    }
+    getivm <- function() ivm
+    print("CacheMatrix Object created, try cacheSolve now.")
+    list(getmtrx = getmtrx, setivm = setivm, getivm = getivm)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Retrieve the inverse of a matrix from CacheMatrix object.
+cacheSolve <- function(x, ...){
+    ivm <- x$getivm()
+    if(is.null(ivm)) {
+        print("Generating&Caching the inverse of the matrix...")
+        mtrx <- x$getmtrx()
+        ivm <- solve(mtrx, ...)
+        x$setivm(ivm)
+    }
+    ivm
 }
